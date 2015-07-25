e66249424110d29aba0c29daa464653662199be6
Nicole isoda <nisoda@hawaii.edu>
2015-07-24 21:10:50 -1000

    Merge conflict

--- Level Seven/src/PassMan.java
+++ Level Seven/src/PassMan.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
@@ -51,12 +50,12 @@ public class PassMan {
 
 			while (rs.next()) {
 			// ID, and MASTER_USER goes here maybe?
-		        String website = rs.getString("Website");
+		        String website = rs.getString("Site");
 		        String username = rs.getString("Username");
-		        String password = rs.getInt("Password");
+		        String password = rs.getString("Password");
 		         
 		        // print the results
-		        System.out.format("%s\n%s\n%s\n%s\n%s\n", "Website: " + site, "Username: " + username, "Password: " + password);
+		        System.out.format("%s\n%s\n%s\n%s\n%s\n", "Website: " + website, "Username: " + username, "Password: " + password);
 				System.out.println();
 			}
 			
@@ -71,89 +70,7 @@ public class PassMan {
 	}
 	
 	public static void main(String[] args) {
-		// Main program where it runs stuff
-		// This is a test push
 		runSql();
 	}
 
 }
-=======
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class PassMan {
-	static int input = 0;
-	static Long starttimeconnect;
-	static Long endtimeconnect;
-	static Long totaltimeconnect;
-	static Long starttime;
-	static Long endtime;
-	static Long totaltime;
-	// We'll have to change this, I assume it's probably the MASTER_USER
-	// and MASTER_PASS verification
-	//static String userInput; //Studio, Title, Type
-	//static int userNumber; //Year, Number of Episodes
-	
-	/**
-	 * Display the entire list
-	 */
-	public static void runSql(){
-		Connection connection = null;
-
-		try {
-			starttimeconnect = System.currentTimeMillis();
-			// Don't forget to change the password, it's at the end of the following line
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PASSMAN", "root", "/**password goes here*/");
-			
-			//end timer
-			endtimeconnect = System.currentTimeMillis();
-			totaltimeconnect = endtimeconnect - starttimeconnect;
-			
-			Statement stmt = null;
-			
-			//start the timer
-			starttime = System.currentTimeMillis();
-			stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("Select * From AnimeTable");
-			ResultSetMetaData rsmd = rs.getMetaData();
-			//end the timer
-			endtime = System.currentTimeMillis();
-			totaltime = endtime - starttime;
-			
-			stmt = connection.createStatement();
-
-			while (rs.next()) {
-			// ID, and MASTER_USER goes here maybe?
-		        String website = rs.getString("Website");
-		        String username = rs.getString("Username");
-		        String password = rs.getInt("Password");
-		         
-		        // print the results
-		        System.out.format("%s\n%s\n%s\n%s\n%s\n", "Website: " + site, "Username: " + username, "Password: " + password);
-				System.out.println();
-			}
-			
-			System.out.println("Result to connect: " + totaltimeconnect + " MS");
-			System.out.println("Result: " + totaltime + " MS");
-
-		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-			return;
-		}
-	}
-	
-	public static void main(String[] args) {
-		// Main program where it runs stuff
-		// This is a test push
-		runSql();
-	}
-
-}
->>>>>>> master
