c8d6ace27585a50911306a21406bb4af4f04445e
Nicole isoda <nisoda@hawaii.edu>
2015-07-24 23:04:56 -1000

    Added login authentication (no security).

--- Level Seven/src/PassMan.java
+++ Level Seven/src/PassMan.java
@@ -6,6 +6,7 @@ import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 public class PassMan {
+	static Connection connection = null;
 	static int input = 0;
 	static Long starttimeconnect;
 	static Long endtimeconnect;
@@ -17,58 +18,94 @@ public class PassMan {
 	// and MASTER_PASS verification
 	//static String userInput; //Studio, Title, Type
 	//static int userNumber; //Year, Number of Episodes
-	
-	/**
-	 * Display the entire list
-	 */
-	public static void runSql(){
-		Connection connection = null;
 
+	public PassMan () {
 		try {
 			starttimeconnect = System.currentTimeMillis();
 			// Don't forget to change the password, it's at the end of the following line
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PASSMAN", "root", "password");
-			
+
 			//end timer
 			endtimeconnect = System.currentTimeMillis();
 			totaltimeconnect = endtimeconnect - starttimeconnect;
-			
-			Statement stmt = null;
-			
-			//start the timer
-			starttime = System.currentTimeMillis();
-			stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("Select * From stored_accounts");
-			ResultSetMetaData rsmd = rs.getMetaData();
-			//end the timer
-			endtime = System.currentTimeMillis();
-			totaltime = endtime - starttime;
-			
-			stmt = connection.createStatement();
-
-			while (rs.next()) {
-			// ID, and MASTER_USER goes here maybe?
-		        String website = rs.getString("Site");
-		        String username = rs.getString("Username");
-		        String password = rs.getString("Password");
-		         
-		        // print the results
-		        System.out.format("%s\n%s\n%s\n%s\n%s\n", "Website: " + website, "Username: " + username, "Password: " + password);
-				System.out.println();
-			}
-			
-			System.out.println("Result to connect: " + totaltimeconnect + " MS");
-			System.out.println("Result: " + totaltime + " MS");
-
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
-			return;
+		}
+	}
+
+	/**
+	 * Display the entire list
+	 */
+	public static void runSql(){
+		if(connection != null) {
+			try {
+				Statement stmt = null;
+
+				//start the timer
+				starttime = System.currentTimeMillis();
+
+				stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery("Select * From stored_accounts");
+				ResultSetMetaData rsmd = rs.getMetaData();
+				//end the timer
+				endtime = System.currentTimeMillis();
+				totaltime = endtime - starttime;
+
+				stmt = connection.createStatement();
+
+				while (rs.next()) {
+					// ID, and MASTER_USER goes here maybe?
+					String website = rs.getString("Site");
+					String username = rs.getString("Username");
+					String password = rs.getString("Password");
+
+					// print the results
+					System.out.format("%s\n%s\n%s\n%s\n%s\n", "Website: " + website, "Username: " + username, "Password: " + password);
+					System.out.println();
+				}
+
+				System.out.println("Result to connect: " + totaltimeconnect + " MS");
+				System.out.println("Result: " + totaltime + " MS");
+
+			} catch (SQLException e) {
+				System.out.println("Connection Failed! Check output console");
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("You are not connected.");
 		}
 	}
 	
-	public static void main(String[] args) {
-		runSql();
+	public static boolean authenticateLogin(String username, String password) {
+		boolean authenticated = false;
+		if(connection != null) {
+			try {
+				Statement stmt = null;
+
+				//start the timer
+				starttime = System.currentTimeMillis();
+
+				stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM USER_LOGINS U WHERE U.USERNAME=\"%s\" AND U.PASSWORD=\"%s\"", username, password));
+				//end the timer
+				endtime = System.currentTimeMillis();
+				totaltime = endtime - starttime;
+
+				if(rs.first()) {
+					authenticated = true;
+				}
+			} catch (SQLException e) {
+				System.out.println("Connection Failed! Check output console");
+				e.printStackTrace();
+			}
+			
+			return authenticated;
+			
+		} else {
+			System.out.println("You are not connected.");
+			return false;
+		}
 	}
 
 }
