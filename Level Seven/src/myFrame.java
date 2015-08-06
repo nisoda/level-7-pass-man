648c9becf1b7e2a385d0144aced50b361f202147
shiroyuki-hime <logawa12@yahoo.com>
2015-08-06 03:52:48 -1000

    Finished the hash and salt.

--- Level Seven/src/myFrame.java
+++ Level Seven/src/myFrame.java
@@ -15,6 +15,8 @@ import javax.swing.JTextField;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -25,7 +27,6 @@ import org.eclipse.wb.swing.FocusTraversalOnArray;
 
 import java.awt.Component;
 import java.sql.SQLException;
-
 
 public class myFrame extends JFrame {
 
@@ -63,7 +64,7 @@ public class myFrame extends JFrame {
 		contentPane.setBackground(new Color(30, 144, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
+
 		pm = new PassMan();
 
 		JLabel lblTitle = new JLabel("Level Seven");
@@ -83,7 +84,7 @@ public class myFrame extends JFrame {
 		txtUsername.setColumns(10);
 
 		pwdPassword = new JPasswordField("Password");
-		pwdPassword.setPreferredSize(new Dimension(274,23));
+		pwdPassword.setPreferredSize(new Dimension(274, 23));
 		pwdPassword.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -97,26 +98,32 @@ public class myFrame extends JFrame {
 		final JLabel errorMessage = new JLabel("");
 		errorMessage.setHorizontalAlignment(SwingConstants.CENTER);
 
-
 		btnLogin.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				username = txtUsername.getText();
-				password = String.valueOf(pwdPassword.getPassword()); 
-				int pass = pm.authenticateLogin(username, password);
-				if(pass == 0){
-					dispose();
-					new passManagerWindow(username);
-				}
-				else if(pass == 1){
-					errorMessage.setText("Username/Password too long");
-				}
-				else{
-					errorMessage.setText("Incorrect password");
+				password = String.valueOf(pwdPassword.getPassword());
+				int pass;
+				try {
+					pass = pm.authenticateLogin(username, password);
+					if (pass == 0) {
+						dispose();
+						new passManagerWindow(username);
+					} else if (pass == 1) {
+						errorMessage.setText("Username/Password too long");
+					} else {
+						errorMessage.setText("Incorrect password");
+					}
+				} catch (NoSuchAlgorithmException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (NoSuchProviderException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
 			}
 		});
-		
+
 		JButton btnNewButton = new JButton("Register");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
@@ -125,56 +132,55 @@ public class myFrame extends JFrame {
 				password = String.valueOf(pwdPassword.getPassword());
 				int pass = -1;
 				try {
-					pass = pm.addUser(username,password);
+					pass = pm.addUser(username, password);
 				} catch (SQLException e1) {
 					e1.printStackTrace();
 				}
-				if(pass == 0){
+				if (pass == 0) {
 					errorMessage.setText("Account has been created");
-				}
-				else if(pass == 3){
+				} else if (pass == 3) {
 					errorMessage.setText("Username has been taken");
 				}
 			}
 		});
 
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-				gl_contentPane.createParallelGroup(Alignment.LEADING)
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-										.addContainerGap()
-										.addComponent(lblTitle, GroupLayout.PREFERRED_SIZE, 405, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createSequentialGroup().addContainerGap().addComponent(
+										lblTitle, GroupLayout.PREFERRED_SIZE, 405, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(71)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(errorMessage, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(pwdPassword)
+										.addComponent(txtUsername, GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)
 										.addGroup(gl_contentPane.createSequentialGroup()
-												.addGap(71)
-												.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-														.addComponent(errorMessage, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-														.addComponent(pwdPassword)
-														.addComponent(txtUsername, GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)
-														.addGroup(gl_contentPane.createSequentialGroup()
-																.addComponent(btnLogin, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE)
-																.addPreferredGap(ComponentPlacement.UNRELATED)
-																.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-																.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-				);
-		gl_contentPane.setVerticalGroup(
-				gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-						.addComponent(lblTitle, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-						.addGap(46)
-						.addComponent(txtUsername, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+												.addComponent(btnLogin, GroupLayout.PREFERRED_SIZE, 133,
+														GroupLayout.PREFERRED_SIZE)
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE,
+														GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
+				.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		gl_contentPane
+				.setVerticalGroup(
+						gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+										.addComponent(lblTitle, GroupLayout.PREFERRED_SIZE, 41,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(46)
+										.addComponent(txtUsername, GroupLayout.PREFERRED_SIZE, 23,
+												GroupLayout.PREFERRED_SIZE)
 						.addGap(18)
-						.addComponent(pwdPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addGap(30)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btnLogin)
-								.addComponent(btnNewButton))
-								.addGap(18)
-								.addComponent(errorMessage)
-								.addContainerGap(18, Short.MAX_VALUE))
-				);
+						.addComponent(pwdPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+				.addGap(30)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(btnLogin)
+						.addComponent(btnNewButton)).addGap(18).addComponent(errorMessage)
+				.addContainerGap(18, Short.MAX_VALUE)));
 		contentPane.setLayout(gl_contentPane);
-		contentPane.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblTitle, txtUsername, pwdPassword, btnLogin, btnNewButton}));
+		contentPane.setFocusTraversalPolicy(new FocusTraversalOnArray(
+				new Component[] { lblTitle, txtUsername, pwdPassword, btnLogin, btnNewButton }));
 	}
 }
